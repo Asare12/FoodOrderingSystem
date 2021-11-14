02a6660f027316eb6bb825ba533da60289191f4e
davidasare <gohan.david11@gmail.com>
2021-11-14 17:22:49 +0000

    feat: Creating product repository

--- src/main/java/com/davidasare/FoodOrderingSystem/repository/ProductRepository.java
+++ src/main/java/com/davidasare/FoodOrderingSystem/repository/ProductRepository.java
@@ -1,2 +1,13 @@
-package com.davidasare.FoodOrderingSystem.repository;public interface ProductRepository {
+package com.davidasare.FoodOrderingSystem.repository;
+
+import com.davidasare.FoodOrderingSystem.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
+    Optional<Product> findByIdAndCategoryId(Long id, Long categoryId);
 }
