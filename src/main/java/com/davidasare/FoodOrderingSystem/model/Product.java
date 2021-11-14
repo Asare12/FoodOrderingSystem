f0f311bc1e122cf78d26378377c8ed84ac0ceb99
davidasare <gohan.david11@gmail.com>
2021-11-14 17:21:40 +0000

    feat: Creating product model

--- src/main/java/com/davidasare/FoodOrderingSystem/model/Product.java
+++ src/main/java/com/davidasare/FoodOrderingSystem/model/Product.java
@@ -1,2 +1,25 @@
-package com.davidasare.FoodOrderingSystem.model;public class Product {
+package com.davidasare.FoodOrderingSystem.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private String price;
+    private String description;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn
+    private Category category;
+
 }
