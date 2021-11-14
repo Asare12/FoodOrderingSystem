b818ee313f4a6ff45c27128232ccc6ac7af361d5
davidasare <gohan.david11@gmail.com>
2021-11-14 17:18:58 +0000

    feat: Creating a one to many relationship with product

--- src/main/java/com/davidasare/FoodOrderingSystem/model/Category.java
+++ src/main/java/com/davidasare/FoodOrderingSystem/model/Category.java
@@ -1,14 +1,14 @@
 package com.davidasare.FoodOrderingSystem.model;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @Entity
 @Table(name = "category")
 public class Category {
@@ -17,9 +17,23 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @OneToMany
+    private List<Product> products;
 
     public Category(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+    public Category() {
+        super();
+        products = new ArrayList<>();
+    }
+
+    public List<Product> getProduct() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
     }
 }
